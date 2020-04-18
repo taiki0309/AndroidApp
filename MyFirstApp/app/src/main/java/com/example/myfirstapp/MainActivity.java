f8026579bf6c6c5153a6a5359a3a0e4b36b5259b
taiki0309 <taiki03090327@gmail.com>
2020-04-18 22:35:55 +0900

    androidアプリ作成

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(android.view.View view){
+        TextView bmiScore = (TextView)this.findViewById(R.id.bmiScore);
+        EditText heightForm = (EditText)this.findViewById(R.id.heightForm);
+        EditText weightForm = (EditText)this.findViewById(R.id.weightForm);
+
+        String strHeight,strWeight;
+        strHeight = heightForm.getText().toString();
+        strWeight = weightForm.getText().toString();
+
+        double bmi,height,weight;
+        height = Double.parseDouble(strHeight);
+        weight = Double.parseDouble(strWeight);
+
+        height /=100;
+        bmi = weight /(height*height);
+        String msg = "BMIスコア:" + bmi;
+
+        bmiScore.setText(msg);
+    }
+}
